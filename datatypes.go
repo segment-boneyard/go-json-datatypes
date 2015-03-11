ccfee58c3845b4372b5ce3944baa2cb512a8b29c
Travis Jeffery <tj@travisjeffery.com>
2015-03-11 02:05:12 -0500

    Lowercase types

--- datatypes.go
+++ datatypes.go
@@ -1,5 +1,14 @@
 package datatypes
 
+// FYI these are the types when you use json.Unmarshal
+//
+// bool, for JSON booleans
+// float64, for JSON numbers
+// string, for JSON strings
+// []interface{}, for JSON arrays
+// map[string]interface{}, for JSON objects
+// nil for JSON null
+
 func DataType(v interface{}) string {
 	if v == nil {
 		return "null"
@@ -7,16 +16,16 @@ func DataType(v interface{}) string {
 
 	switch v.(type) {
 	case int, int8, int32, int64, uint, uint8, uint32, uint64, float32, float64:
-		return "Number"
+		return "number"
 	case string:
-		return "String"
+		return "string"
 	case bool:
-		return "Boolean"
+		return "boolean"
 	case []interface{}, []string, []int, []int8, []int32, []int64, []uint, []uint8, []uint32, []uint64, []float32, []float64:
-		return "Array"
+		return "array"
 	case map[interface{}]interface{}, map[string]interface{}:
-		return "Object"
+		return "object"
 	default:
-		return "Value"
+		return "value"
 	}
 }
--- datatypes_test.go
+++ datatypes_test.go
@@ -4,10 +4,10 @@ import "github.com/bmizerany/assert"
 import "testing"
 
 func TestDatatype(t *testing.T) {
-	assert.Equal(t, "Boolean", DataType(true))
-	assert.Equal(t, "Number", DataType(16))
-	assert.Equal(t, "String", DataType("hello world"))
-	assert.Equal(t, "Array", DataType([]string{"hello", "world"}))
-	assert.Equal(t, "Object", DataType(map[string]interface{}{"hello": "world"}))
+	assert.Equal(t, "boolean", DataType(true))
+	assert.Equal(t, "number", DataType(16))
+	assert.Equal(t, "string", DataType("hello world"))
+	assert.Equal(t, "array", DataType([]string{"hello", "world"}))
+	assert.Equal(t, "object", DataType(map[string]interface{}{"hello": "world"}))
 	assert.Equal(t, "null", DataType(nil))
 }
